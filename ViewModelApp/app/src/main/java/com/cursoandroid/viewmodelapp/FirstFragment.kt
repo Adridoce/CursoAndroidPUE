7b8c77385a7f99312d6a1626a9930e68afcbf8cb
Adridoce <adrian.perezurrialde@gmail.com>
2024-02-19 22:01:58 +0100

    viewModel

---
+++ ViewModelApp/app/src/main/java/com/cursoandroid/viewmodelapp/FirstFragment.kt
@@ -0,0 +1,31 @@
+package com.cursoandroid.viewmodelapp
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.cursoandroid.viewmodelapp.databinding.FragmentFirstBinding
+
+class FirstFragment : Fragment() {
+
+    private lateinit var binding: FragmentFirstBinding
+    private lateinit var viewModel: FirstViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout for this fragment
+        binding = FragmentFirstBinding.inflate(inflater, container, false)
+
+        viewModel = ViewModelProvider()
+
+
+        return binding.root
+    }
+
+
+
+}
---
+++ ViewModelApp/app/src/main/java/com/cursoandroid/viewmodelapp/FirstViewModel.kt
@@ -0,0 +1,7 @@
+package com.cursoandroid.viewmodelapp
+
+import androidx.lifecycle.ViewModel
+
+class FirstViewModel : ViewModel() {
+
+}
